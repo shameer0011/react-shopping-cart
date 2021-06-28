06c75b9f766a238696594a8815b3b16b00f71d60
shameer <shameer0011@github.com>
2021-06-28 20:48:02 +0530

    Display products and styles

--- react-shopping-cart/src/components/main/mainStyle.js
+++ react-shopping-cart/src/components/main/mainStyle.js
@@ -1,7 +1,15 @@
 import { makeStyles } from "@material-ui/styles";
 
 export const useStyles = makeStyles(theme => ({
-  content: {
-    height: "100vh",
+  media: {
+    height: 340,
+  },
+  buttonStyle: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  prizeStyles: {
+    color: "black",
+    fontWeight: "bold",
   },
 }));
